658f766a4e496d6577dde841c53b349522388ca1
Nikita <kuzmenko225@gmail.com>
2024-08-14 22:19:53 +0300

    Practical-work

--- module_1_3.py
+++ module_1_3.py
@@ -1,8 +1,8 @@
-name = str("Name: Nikita")
+name = str("Nikita")
 print(name)
-age = 35
+age = 27
 print(age)
-New_age = 27
-print(age+1)
-is_student = bool (True)
+new_age = 27
+print(new_age + 1)
+is_student = bool(True)
 print(is_student)
