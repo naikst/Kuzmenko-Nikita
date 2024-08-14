7eaac62457319a4ac7dd09771f72ea6ce2590787
Nikita <kuzmenko225@gmail.com>
2024-08-14 16:43:53 +0300

    Practical-wrok

--- scratch.py
+++ scratch.py
@@ -1,6 +1,5 @@
-a = 'Курс: Python,'
-b = 'всего задач:12,'
-c = 'затрачено часов: 1.5,'
-print(1.5/12)
-k = "среднее время выполнение 0.125 часа."
-print(a,b,c,k)
+number_of_completed = 12
+number_of_hours = 1.5
+course = "Python"
+time_for_one_task = number_of_hours / number_of_completed
+print("Курс: " + str(course) + ", всего задач: " + str(number_of_completed) + ", всего часов: " + str(number_of_hours) + ", средняя время выполнения "  + str(time_for_one_task) + " часов.")
