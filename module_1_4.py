601aa29241f317eaa8150dc1d29610aa7ef437a5
Nikita <kuzmenko225@gmail.com>
2024-08-15 20:48:46 +0300

    Practical-work

--- module_1_4.py
+++ module_1_4.py
@@ -7,8 +7,8 @@ for char in string:
     print("Количество символов в строке:", count)
 print('my_string'.upper())
 print('my_string'.lower())
-print("my_string" .replace('my_string', 'mystring'))
-print("my_string".replace('my_string', "m"))
-print("my_string".replace('my_string', "g"))
+print('my_string' .replace('my_string', 'mystring'))
+print('my_string'.replace('my_string', 'm'))
+print('my_string'.replace('my_string', 'g'))
 
 
