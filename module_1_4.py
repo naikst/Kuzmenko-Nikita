db2f672996be1dffa71ce8295008ce95497cb518
Nikita <kuzmenko225@gmail.com>
2024-08-15 17:07:47 +0300

    Practical-work

---
+++ module_1_4.py
@@ -0,0 +1,10 @@
+my_string = input('Введите текст: ')
+print(len(my_string))
+
+print("my_string".upper())
+print("my_string".lower())
+print('my_string'.replace('my_string', "mystring"))
+print('my_string'.replace('my_string', "m"))
+print('my_string'.replace('my_string', "g"))
+
+
