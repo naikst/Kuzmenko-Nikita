b7cba5dcb81b861027b79d580bb88370c0f0510c
Nikita <kuzmenko225@gmail.com>
2024-08-16 16:57:03 +0300

    Practical-work

--- module_1_4.py
+++ module_1_4.py
@@ -1,10 +1,14 @@
 name = input('Введите строку: ')
 print(type(name))
+
 string = 'my_string'
 print(len(string))
+
 print('my_string'.upper())
 print('my_string'.lower())
+
 print('my_string'.replace("_", ""))
+
 print(name[0])
 print(name[-1])
 
