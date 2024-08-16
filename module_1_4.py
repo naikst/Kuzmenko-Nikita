531a37678ceadad774abdd81639ac7c9b1346310
Nikita <kuzmenko225@gmail.com>
2024-08-16 12:27:38 +0300

    Practical-work

--- module_1_4.py
+++ module_1_4.py
@@ -1,14 +1,17 @@
 name = input('my_string')
 print(type(name))
-string = "my_string"
-count = 0
-for char in string:
-    count += 1
-    print("Количество символов в строке:", count)
+
+string = 'my_string'
+print(len(string))
+
 print('my_string'.upper())
 print('my_string'.lower())
+
 print('my_string' .replace('my_string', 'mystring'))
-print('my_string'.replace('my_string', 'm'))
-print('my_string'.replace('my_string', 'g'))
+
+name = 'my_string'
+print(name[0])
+print(name[-1])
+
 
 
