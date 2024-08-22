b2415d821d33b99d2c266f7579c0c9e464b933a1
Nikita <kuzmenko225@gmail.com>
2024-08-22 09:08:04 +0300

    Practical-work

--- module_2_3.py
+++ module_2_3.py
@@ -1,7 +1,7 @@
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 index = 0
 elem = my_list[index]
-while elem >= 0 and index < len(my_list):
+while index < len(my_list):
     elem = my_list[index]
     index += 1
     if elem < 0:
