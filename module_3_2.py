7778560d92812da83636e0409f47d00180b4a16f
Nikita <kuzmenko225@gmail.com>
2024-09-02 19:10:52 +0300

    Practical-work

--- module_3_2.py
+++ module_3_2.py
@@ -1,31 +1,13 @@
-def send_email(message, recipient, *, sender='urban.teacher@mail.ru'):
-    # 1 "message" и "recipient" - позиционные
-    # аргументы указываем при вызове функций. sender -  именнованный аргумент, который по умолчанию имеет зачение
-    # "university.help@gmail.com". Символ * перед sender делает его обязательным именованным аргументом,
-    # который можно указать только по имени.
-    # 2. Проверка корректности e-mail адресов/ проверить, содержат ли строки recipient и sender
-    # Проверка на корректность адреса получателя
-    if '@' not in recipient or not (
-            recipient.endswith('.com') or recipient.endswith('.ru') or recipient.endswith('.net')):
+def send_email(message: str, recipient: str, sender="university.help@gmail.com"):
+    if "@" not in recipient or "@" not in sender \
+            or not recipient.endswith((".com", ".ru", ".net")) or not sender.endswith((".com", ".ru", ".net")):
         print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
-        return
-
-    # Проверка на корректность адреса отправителя
-    if '@' not in sender or not (
-            sender.endswith('.com') or sender.endswith('.ru') or sender.endswith('.net')):
-        print(f"Невозможно отправить письмо с адреса {sender} на адрес {recipient}")
-        return
-
-    # Проверка на отправку самому себе
-    if sender == recipient:
+    elif recipient == sender:
         print("Нельзя отправить письмо самому себе!")
-        return
-
-    # Проверка на отправителя
-    if sender == 'urban.teacher@mail.ru':
-        print(f"Письмо успешно отправлено с адреса {sender} на адрес {recipient}")
+    elif sender != "university.help@gmail.com":
+        print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}.")
     else:
-        print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса {sender} на адрес {recipient}")
+        print("Письмо успешно отправлено с адреса <sender> на адрес <recipient>.")
 
 
 # Примеры вызова функции
