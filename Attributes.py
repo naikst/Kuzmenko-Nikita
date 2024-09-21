4147f18f638eb3eae195e1d56bc2e0b9d2f145f6
Nikita <kuzmenko225@gmail.com>
2024-09-21 21:05:09 +0300

    Задача "Магические здания"

--- Attributes.py
+++ Attributes.py
@@ -1,29 +1,50 @@
-class Human:# Класс Human, который описывает человека.
-    def __init__(self, name, age): # Это специальный метод, который вызывается при создании объекта. Он
+class Human:  # Класс Human, который описывает человека.
+
+    head = True
+
+    def __init__(self, name, age):  # Это специальный метод, который вызывается при создании объекта. Он
         # инициализирует атрибуты объекта.
 
-        #Атрибуты - это переменные, принадлежащие объекту. Они хранят данные об объекте.
+        # Атрибуты - это переменные, принадлежащие объекту. Они хранят данные об объекте.
         self.name = name
         self.age = age
         self.say_info()
-        #self - это специальный параметр в методах класса, который указывает на текущий объект.
+        # self - это специальный параметр в методах класса, который указывает на текущий объект.
 
     def say_info(self):  # это методы класса Human.
         print(f'Привет, меня зовут {self.name}, мне {self.age}')
 
-    def birthday(self):  #это методы класса Human.
+    def birthday(self):  # это методы класса Human.
         self.age += 1
         print(f'Мне исполнилось {self.age} лет')
 
-    def change_name(self, new_name):  #это методы класса Human.
-        old_name = self.name #ставим старое имя
-        self.name = new_name #заменим имя на новое
-        print(f'Предыдущее имя: {old_name}, новое имя: {self.name}')
+    def _str__(self):  # это специальный метод, который вызывается при преобразовании объекта в строку.
+        return f'{self.name}'  # Возвращает строку с информацией о человеке.
+
+    def __len__(self):
+        return self.age  # Возвращает возраст человека.
+
+    def __lt__(self, other):
+        return self.age < other.age  # Возвращает True, если возраст текущего человека меньше возраста other, иначе False.
+
+    def __gt__(self, other):
+        return self.age > other.age  # Возвращает True, если возраст текущего человека больше возраста other, иначе False.
+
+    def __eq__(self, other):
+        return self.age == other.age  # Возвращает True, если возраст текущего человека ра��ен возрасту other, иначе False.
+
+    def __bool__(self):
+        return bool(self.age > 0)  # Возвращает True, если возраст человека положительный, иначе False.
+
+    def __del__(self):
+        print(f'{self.name} ушёл')  # Выводит сообщение об удалении объекта.
 
 
 
-#Создание и использование объектов:
+
+# Создание и использование объектов:
 den = Human('Денис', 24)
 max = Human('Максим', 25)
-max.change_name('Михаил')
-max.birthday()
+a = 6
+
+print(Human.head)
